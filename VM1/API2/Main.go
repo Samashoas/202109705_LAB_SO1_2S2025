55918fe985717131c6d12899e6063c0d47359dc1
Samashoas <3664015720115@ingenieria.usac.edu.gt>
2025-08-16 06:55:02 +0000

    feat: Endpoints Api2

---
+++ VM1/API2/Main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+const (
+	ApiName = "API2"
+	VmName = "VM1"
+	Estudiante = "Juan Pablo Samayoa Ruiz"
+	Carnet = "202109705"
+	port = "8080"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	http.HandleFunc("/api2/"+Carnet+"/llamar-api1", func(w http.ResponseWriter, r *http.Request) {
+		writeJson(w, Response{Message: "Hola, responde la API2: " + ApiName + " en la " + VmName + ", desarrollada por el estudiante: " + Estudiante + " con carnet: " + Carnet})
+	})
+
+	http.HandleFunc("/api2/"+Carnet+"/llamar-api3", func(w http.ResponseWriter, r *http.Request) {
+		writeJson(w, Response{Message: "Hola, responde la API3: " + ApiName + " en la " + VmName + ", desarrollada por el estudiante: " + Estudiante + " con carnet: " + Carnet})
+	})
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+
+	http.ListenAndServe(":"+port, nil)
+}
+
+func writeJson(w http.ResponseWriter, response Response) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
